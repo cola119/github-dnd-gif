9eaa6dd49ead83a70bd3cb5d8921ed42e259d384
Kohei Ueno <k.cola119@ezweb.ne.jp>
2020-11-19 23:05:42 +0900

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import { ConfigurationFactory } from "webpack";
+import path from "path";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+
+const config: ConfigurationFactory = () => {
+  return {
+    entry: {
+      background: path.join(__dirname, "src", "background.ts"),
+      popup: path.join(__dirname, "src", "popup.ts"),
+    },
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "[name].js",
+    },
+    module: {
+      rules: [
+        {
+          test: /.ts$/,
+          use: "ts-loader",
+          exclude: "/node_modules/",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [{ from: "public", to: "." }],
+      }),
+    ],
+  };
+};
+
+export default config;
